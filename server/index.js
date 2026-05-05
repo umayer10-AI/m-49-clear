adfd6637369585722d49cca14b70ea3db84b41bc
umayer10-AI <mdumayer10@gmail.com>
2026-05-05 19:19:31 +0600

    add

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
 
 const users = [
     {id: 1, name: "Umayer", email: "jhon@example.com"},
@@ -12,6 +16,17 @@ const users = [
 
 app.get('/', (req, res) => {
   res.send(users)
+})
+
+app.post('/', (req, res) => {
+
+    console.log("server data paisi",req.body)
+
+    const newUser = req.body
+    newUser.id = users.length+1
+    users.push(newUser)
+
+    res.send({ok: true, data: newUser, message: "Data paisi re vai😎"})
 })
 
 app.get('/about', (req, res) => {
